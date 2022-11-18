d3d505fd0f8e2ed724d4783af4fcb9f0af69f2a5
siddarthvadr <sddhrthmall40@gmail.com>
2022-11-18 23:34:50 +0530

    Backend Working(predictions)

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
 from flask import Flask, request, jsonify
+import util
 
 app = Flask(__name__)
 
-app.route('/hello')
-def hello():
-  return 'Hello' 
+@app.route('/classify_image', methods=['GET', 'POST'])
+def classify_image():
+  image_data = request.form['image_data']
+  
+  response = jsonify(util.classify_image(image_data))
+
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  
+  return response
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    print('Starting Server for Classification Model')
+    util.load_saved_artifacts
+    app.run(port = 5000)
