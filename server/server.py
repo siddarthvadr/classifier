ebbafe21d0301c359e4143266612d7ed60da2074
siddarthvadr <sddhrthmall40@gmail.com>
2022-11-21 00:37:30 +0530

    Final working

--- server/server.py
+++ server/server.py
@@ -3,17 +3,18 @@ import util
 
 app = Flask(__name__)
 
+
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
-  image_data = request.form['image_data']
-  
-  response = jsonify(util.classify_image(image_data))
+    image_data = request.form['image_data']
 
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  
-  return response
+    response = jsonify(util.classify_image(image_data))
 
-if __name__ == '__main__':
-    print('Starting Server for Classification Model')
-    util.load_saved_artifacts
-    app.run(port = 5000)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    util.load_saved_artifacts()
+    app.run(port=5000)
